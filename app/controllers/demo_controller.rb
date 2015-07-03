66e3ba030bf677ffce16c6f4569410a7a98e335d
adutka <adutka@ukr.net>
2015-07-03 11:58:05 +0300

    5_2 redirecting actions

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -8,4 +8,12 @@ class DemoController < ApplicationController
   def hello
     render('index')
   end
+
+  def other_hello
+    redirect_to(:controller =>'demo', :action => 'index')
+  end
+
+  def lynda
+    redirect_to("http://lynda.com")
+  end
 end
