38f9222ebf7888340b48e14bd2158e8410d77f7f
adutka <adutka@ukr.net>
2015-07-03 11:41:37 +0300

    5_1 rendering template

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,11 @@
 class DemoController < ApplicationController
   layout false
+
   def index
+    render(:template => 'demo/hello') # or render('demo/hello')
+  end
+
+  def hello
+    render('index')
   end
 end
