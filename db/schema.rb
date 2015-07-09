d41cead27604526eaa77edc7e183ee7f61fb9ece
adutka <adutka@ukr.net>
2015-07-09 21:49:49 +0300

    solution migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709171033) do
+ActiveRecord::Schema.define(version: 20150709174048) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "first_name",      limit: 25
@@ -24,5 +24,39 @@ ActiveRecord::Schema.define(version: 20150709171033) do
   end
 
   add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
+
+  create_table "pages", force: :cascade do |t|
+    t.integer  "suject_id",  limit: 4
+    t.string   "name",       limit: 255
+    t.string   "permalink",  limit: 255
+    t.integer  "position",   limit: 4
+    t.boolean  "visible",    limit: 1,   default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+  end
+
+  add_index "pages", ["permalink"], name: "index_pages_on_permalink", using: :btree
+  add_index "pages", ["suject_id"], name: "index_pages_on_suject_id", using: :btree
+
+  create_table "sections", force: :cascade do |t|
+    t.integer  "page_id",      limit: 4
+    t.string   "name",         limit: 255
+    t.integer  "position",     limit: 4
+    t.boolean  "visible",      limit: 1,     default: false
+    t.string   "context_type", limit: 255
+    t.text     "content",      limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sections", ["page_id"], name: "index_sections_on_page_id", using: :btree
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "position",   limit: 4
+    t.boolean  "visible",    limit: 1,   default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
