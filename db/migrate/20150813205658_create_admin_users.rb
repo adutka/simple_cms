dd5706714eabdd70c7c484e4cc9976b31e1f36ab
adutka <adutka@ukr.net>
2015-08-15 00:42:05 +0300

    many_to_many_rich

--- db/migrate/20150813205658_create_admin_users.rb
+++ db/migrate/20150813205658_create_admin_users.rb
@@ -1,8 +1,18 @@
 class CreateAdminUsers < ActiveRecord::Migration
-  def change
+  def up
     create_table :admin_users do |t|
-
-      t.timestamps null: false
+      t.string "first_name", limit: 25
+      t.string "last_name", limit: 50
+      t.string "email", limit: 100, default: ""
+      t.string "username", limit: 25
+      t.string "hashed_password", limit: 40
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
     end
+    add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
+  end
+
+  def down
+    drop_table :admin_users
   end
 end
